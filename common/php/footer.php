feb10de05e18e14509b959805702131822ec055b
Ariub197 <arisumi19702@gmail.com>
2024-03-14 16:23:53 +0900

    [update]default作成

--- common/php/footer.php
+++ common/php/footer.php
@@ -1,4 +1,10 @@
-<script src="./common/js/plugins.min.js"></script>
+<foote id="footer" class="c-footer">
+  d
+</foote>
+
+<a href="#" class="c-btn__page-top" date-btn="top"></a>
+
+  <script src="./common/js/plugins.min.js"></script>
   <script src="./common/js/common.min.js" defer></script>
 </body>
 </html>
--- common/php/header.php
+++ common/php/header.php
@@ -6,20 +6,39 @@
   <title>title</title>
   <link rel="stylesheet" href="./common/css/style.min.css">
 </head>
-<body>
-  <header id="header">
-    <h1 class="c-header__logo">
-      <img src="/common/img/logo/logo_header.png" alt="ロゴ画像">
-    </h1>
 
-    <nav class="c-header-nav">
-      <ul class="c-header-nav__list">
-        <li class="c-header-nav__item"><a href="" class="c-header-nav__link">about</a></li>
-        <li class="c-header-nav__item"><a href="" class="c-header-nav__link">about</a></li>
-        <li class="c-header-nav__item"><a href="" class="c-header-nav__link">about</a></li>
-        <li class="c-header-nav__item"><a href="" class="c-header-nav__link">about</a></li>
-        <li class="c-header-nav__item"><a href="" class="c-header-nav__link">about</a></li>
-        <li class="c-header-nav__item"><a href="" class="c-header-nav__link">about</a></li>
-      </ul>
-    </nav>
+<body>
+  <header id="header" class="c-header">
+    <div class="c-header__inner">
+      <h1 class="c-header__logo">
+        <a href="" class="c-header__logo-link"><img src="/common/img/logo/logo_header.png" alt="ロゴ画像" class="c-header__logo-img"></a>
+      </h1>
+
+      <!-- header__hamburger start -->
+      <div class="c-header__hamburger">
+        <div class="c-btn-hamburger" data-nav="btn">
+          <span class="c-btn-hamburger__line"></span>
+          <span class="c-btn-hamburger__line"></span>
+          <span class="c-btn-hamburger__line"></span>
+        </div>
+      </div>
+      <!-- header__hamburger end -->
+
+      <!-- header-nav start -->
+      <div class="c-nav" data-nav="content">
+        <nav class="c-header-nav">
+          <ul class="c-header-nav__list">
+            <li class="c-header-nav__item"><a href="" class="c-header-nav__link">about</a></li>
+            <li class="c-header-nav__item"><a href="" class="c-header-nav__link">about</a></li>
+            <li class="c-header-nav__item"><a href="" class="c-header-nav__link">about</a></li>
+            <li class="c-header-nav__item"><a href="" class="c-header-nav__link">about</a></li>
+            <li class="c-header-nav__item"><a href="" class="c-header-nav__link">about</a></li>
+            <li class="c-header-nav__item"><a href="" class="c-header-nav__link">about</a></li>
+          </ul>
+        </nav>
+        <div class="c-header__contact"><a href="/contact/" class="c-btn__contact">お問い合わせ</a></div>
+      </div>
+      <!-- header-nav end -->
+    </div>
+    <div class="c-nav__bg" data-nav="bg"></div>
   </header>
